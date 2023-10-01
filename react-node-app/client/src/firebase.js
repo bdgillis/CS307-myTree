2bc0277e99c3e037880b10992cb3e5e0e2e2a342
Annalise Kimura <annalisekimura@pal-nat186-101-238.itap.purdue.edu>
2023-10-01 16:56:44 -0400

    createUserAndPass test

---
+++ react-node-app/client/src/firebase.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuk5etQpT4_dMQJrbt1yYSeIRLaywRhOE",
+  authDomain: "cs307-mytree.firebaseapp.com",
+  projectId: "cs307-mytree",
+  storageBucket: "cs307-mytree.appspot.com",
+  messagingSenderId: "206348991152",
+  appId: "1:206348991152:web:b483558bcecb1caa58d975",
+  measurementId: "G-G8E9VN99YD"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+export const auth = getAuth(app);
