4eab93b37cd76ede1775770e9fad6e516d37f552
bdgillis <gillisbriand@gmail.com>
2023-10-03 17:50:17 -0400

    Added google auth login button to page

--- react-node-app/client/src/App.js
+++ react-node-app/client/src/App.js
@@ -1,82 +1,109 @@
 import React, { useState } from 'react'
 import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword } from "firebase/auth";
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup
+} from "firebase/auth";
 
-import {auth} from './firebase';
+import { auth } from './firebase';
 
 function App() {
 
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
 
-  const [user, setUser] = useState({});
+    const [user, setUser] = useState({});
 
-  
-  const register = async () => {
-    try {
-      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
-      console.log(user)
 
-    } catch (error) {
-      console.log(error.message);
+    const register = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+            console.log(user)
+
+        } catch (error) {
+            console.log(error.message);
+        }
+
     }
 
-  }
+    const login = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+            console.log(user)
 
-  const login = async () => {
-    try {
-      const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-      console.log(user)
-
-    } catch (error) {
-      console.log(error.message);
+        } catch (error) {
+            console.log(error.message);
+        }
     }
-  }
 
-  return (
-    <div className="App">
+    const googleAuth = async () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user);
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+            });
+    }
 
-      <div>
-        <h3> Register User </h3>
-      
-        <input
-          placeholder="Email..." 
-          onChange={(event) => {
-            setRegisterEmail(event.target.value);
-          }} />
-        <input
-          placeholder="Password..."
-          onChange={(event) => {
-            setRegisterPassword(event.target.value);
-          }} />
+    return (
+        <div className="App">
 
-        <button onClick={register}> Create User</button>
-      </div>
+            <div>
+                <h3> Register User </h3>
 
-      <div>
-        <h3> Login </h3>
+                <input
+                    placeholder="Email..."
+                    onChange={(event) => {
+                        setRegisterEmail(event.target.value);
+                    }} />
+                <input
+                    placeholder="Password..."
+                    onChange={(event) => {
+                        setRegisterPassword(event.target.value);
+                    }} />
 
-        <input
-          placeholder="Email..." 
-          onChange={(event) => {
-            setLoginEmail(event.target.value);
-          }} />
-        <input
-          placeholder="Password..."
-          onChange={(event) => {
-            setLoginPassword(event.target.value);
-          }} />
+                <button onClick={register}> Create User</button>
+            </div>
 
-        <button onClick={login}> Login</button>
-      </div>
+            <div>
+                <h3> Login </h3>
 
-    </div>
+                <input
+                    placeholder="Email..."
+                    onChange={(event) => {
+                        setLoginEmail(event.target.value);
+                    }} />
+                <input
+                    placeholder="Password..."
+                    onChange={(event) => {
+                        setLoginPassword(event.target.value);
+                    }} />
 
-    
-  )
+                <button onClick={login}> Login</button>
+                <button onClick={googleAuth}>Login with Google</button>
+            </div>
+
+        </div>
+
+
+    )
 }
 
 export default App
