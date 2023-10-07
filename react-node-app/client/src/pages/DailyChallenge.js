975aaadc37dc256d7de80489655af07a7336ef17
Annalise Kimura <annalisekimura@Annalises-MacBook-Air.local>
2023-10-06 21:57:22 -0400

    testing merge

---
+++ react-node-app/client/src/pages/DailyChallenge.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DailyChallenge = () => {
+  	return (
+    	<div style={{ 
+        	display: 'flex', 
+        	justifyContent: 'center', 
+        	alignItems: 'center',
+        	height: '90vh'
+        }}>
+        	<h1>Daily Challenge</h1>
+    	</div>
+  	)
+}
+
+export default DailyChallenge
