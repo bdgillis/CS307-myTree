029f98a93ebce590aee97df33400f156b69617ca
bdgillis <gillisbriand@gmail.com>
2023-10-12 12:37:45 -0400

    Update HomeTab.js

--- react-node-app/client/src/pages/HomeTab.js
+++ react-node-app/client/src/pages/HomeTab.js
@@ -11,14 +11,14 @@ const HomeTab = () => {
 
     const auth = getAuth();
     const user = auth.currentUser;
-    if (user) {
-        // console.log(user)
-        const name = user.displayName;
-        // console.log(name);
-        document.getElementById("welcome-msg").innerHTML = "Welcome to myTree  <br/> " + name;
-    } else {
-        document.getElementById("welcome-msg").innerHTML = "Welcome to myTree ";
-    }
+    window.onload = (event) => {
+        if (user) {
+            // console.log(user)
+            const name = user.displayName;
+            // console.log(name);
+            document.getElementById("welcome-msg").innerHTML = "Welcome to myTree  <br/> " + name;
+        }    
+    };
 
 
     return (
@@ -26,16 +26,15 @@ const HomeTab = () => {
 
         <div className="testNav">
             <Sidebar isOpen={isOpen} toggle={toggle} />
-            <Navbar toggle={toggle} />
 
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '90vh'
-            }}>
-                <h1 id="welcome-msg">Home</h1>
+            <div style={{ 
+				position: 'fixed', 
+				top: 0, 
+				left: '12%', 
+				}}>
+                <Navbar toggle={toggle} />
             </div>
+            <h1 id="welcome-msg">Welcome to myTree</h1>
 
         </div>
     )
