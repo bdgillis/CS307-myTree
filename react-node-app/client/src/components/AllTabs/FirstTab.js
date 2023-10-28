0cbb54ea1028ecb4f9a4472b310cd136cc13fb4d
Annalise Kimura <annalisekimura@pal-nat186-100-112.itap.purdue.edu>
2023-10-28 18:07:47 -0400

    work on leaderboard

--- react-node-app/client/src/components/AllTabs/FirstTab.js
+++ react-node-app/client/src/components/AllTabs/FirstTab.js
@@ -9,19 +9,21 @@ const FirstTab = () => {
   const [loadingState, setLoadingState] = useState(true);
   const [allData, setAllData] = useState(true);
   const [allData2, setAllData2] = useState(true);
-  const [scoreDataArr, setScoreDataArr] = useState(true);
+  const [answer, setAnswer] = useState(null);
+
   const scoreData = [];
 
   var uid = "null";
 
   const auth = getAuth();
   const user = auth.currentUser;
+  const [final, setFinal] = useState(null);
 
  
-
+  var i = 0;
   useEffect(() => {
-    let i = 0;
-    async function getProfileData(uidComp) {
+
+    async function getProfileData(uidComp, curUser) {
       if (user) {
           if (user.uid != null){
               uid = user.uid
@@ -36,18 +38,32 @@ const FirstTab = () => {
                   throw new Error('Network response was not ok');
               }
               const profileData = await response.json();
-              setProfileData(profileData); // Set the data in the component's state
-              scoreData[i++] = profileData.carbonScore;
-              setScoreDataArr({scoreData});
 
+              setProfileData(profileData); // Set the data in the component's state
+              const couple = [curUser, profileData.carbonScore];
+              scoreData[i++] = couple;
+              
+
+              
           } catch (error) {
-              console.error('There was an error:', error);
+            if(!profileData){
+              return;
+            }
+            profileData.carbonScore = 0;
+            const couple = [curUser, profileData.carbonScore];
+            scoreData[i++] = couple;
+            console.error('There was an error:', error);
           }
         }
     }
 
       const getAllData = async () => {
         try {
+          setFinal(0);
+          if(final === 1) {
+            return;
+          }
+          setFinal(1);
           const responseOne = await fetch(`/api/FirstTab/`, {
             method: 'GET'
           });
@@ -61,45 +77,28 @@ const FirstTab = () => {
           setLoadingState(false);
 
           allData.users.forEach((userRecord) => {
-            console.log(userRecord.uid);
-            getProfileData(userRecord.uid);
-
-            
+            getProfileData(userRecord.uid, userRecord.displayName);
           });
 
         } catch (error) {
+          
           console.error('There was an error:', error);
         }
       };
       getAllData();
-      console.log(scoreDataArr);
+      setAnswer(scoreData);
 
   }, []); // The empty dependency array ensures that useEffect runs only once
 
   return (
+
  
     <div className="FirstTab">
       <p>Users</p>
       {/* First tab content will go here */}
 
-      {profileData ? (
-                    <h3>Carbon Score: {profileData.carbonScore}</h3>
-                    ) : (
-                    <h3>Carbon Score: Unavailable</h3>
-                )}    
-
-      <html>
-      <body>
-        <pre id ="arrPrint"></pre>
-        <script>
-
-            document.getElementById("arrPrint").innerHTML = JSON.stringify({scoreData}, null, 4);
-        </script>
-    
-
-      </body>
-      </html>
-      <h3>Carbon Scores: {scoreData}</h3>          
+                <h3>Carbon Scores:</h3>
+                <h3>{answer}</h3>
 
 
     </div>
