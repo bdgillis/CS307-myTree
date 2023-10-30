de1912c65fb18fcb9e4758c52204ed62b9864c92
RKOKA209 <48864636+RKOKA209@users.noreply.github.com>
2023-10-29 20:31:28 -0400

    Created tabs for groups page

---
+++ react-node-app/client/src/components/GroupsComponents/GroupsTabs.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import FirstTab from "./SearchTab";
+import SecondTab from "./CreateTab";
+import ThirdTab from "../AllTabs/ThirdTab";
+
+
+
+const GroupsTabs = () => {
+  const [activeTab, setActiveTab] = useState("tab1");
+
+  //  Functions to handle Tab Switching
+  const handleTab1 = () => {
+    // update the state to tab1
+    setActiveTab("tab1");
+  };
+  const handleTab2 = () => {
+    // update the state to tab2
+    setActiveTab("tab2");
+  };
+
+  return (
+    <div className="Tabs">
+      {/* Tab nav */}
+      <ul className="nav">
+        <li className={activeTab === "tab1" ? "active" : ""}
+            onClick={handleTab1}
+        >
+            Tab 1
+        </li>
+
+        <li className={activeTab === "tab2" ? "active" : ""}
+            onClick={handleTab2}
+        >
+            Tab 2
+        </li>
+        
+      </ul>
+      <div className="outlet">
+        {activeTab === "tab1" ? <FirstTab /> : 
+        activeTab === "tab2" ? <SecondTab /> : <ThirdTab />}
+      </div>
+
+    </div>
+  );
+};
+export default GroupsTabs;
