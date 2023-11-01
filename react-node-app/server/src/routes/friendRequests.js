33b93b09d0d49377e4655aa41f6230176ceee457
paulkraessig <pkraessi@purdue.edu>
2023-10-31 21:01:14 -0400

    more api routes

---
+++ react-node-app/server/src/routes/friendRequests.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const calcScore = require('../carbonScore.js')
+const admin = require("firebase-admin");
+const { getFirestore, Timestamp, FieldValue, Filter} = require('firebase-admin/firestore');
+const db = getFirestore();
+const { query, where, getDocs } = require('firebase-admin/firestore');
+
+module.exports = router;
