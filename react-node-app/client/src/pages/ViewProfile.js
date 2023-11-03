0377ef841f843a240a375f64d7495b99b1e7f552
RKOKA209 <48864636+RKOKA209@users.noreply.github.com>
2023-11-03 07:27:09 -0400

    Commenting out my backend changes

--- react-node-app/client/src/pages/ViewProfile.js
+++ react-node-app/client/src/pages/ViewProfile.js
@@ -149,8 +149,6 @@ const ViewProfile = () => {
 
         </>
 
-
-
     )
 }
 
--- react-node-app/server/src/routes/activities.js
+++ react-node-app/server/src/routes/activities.js
@@ -37,6 +37,8 @@ router.post('/', async (req, res) => {
 
 
         const score = calcScore(req.body.activeCategory, req.body.activeActivity, req.body.activityParam);
+
+
         
         const userRef = await db.collection('users').doc(req.body.uid).update({
             numActivities: numAct + 1,
@@ -57,9 +59,9 @@ router.post('/', async (req, res) => {
 
 module.exports = router;
 
-exports.resetWeekly = functions.pubsub.schedule('5 * * * *').onRun((context) => {
-    console.log('This will be run every 5 minutes!');
-    weeklyNumActivities: 0;
-    weeklyCarbonScore: 0;
-    return null;
-})
+// exports.resetWeekly = functions.pubsub.schedule('5 * * * *').onRun((context) => {
+//     console.log('This will be run every 5 minutes!');
+//     weeklyNumActivities: 0;
+//     weeklyCarbonScore: 0;
+//     return null;
+// })
